af4700bb809cbc38f51c0044db48071230238b35
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-18 18:47:27 +0200

    ...

---
+++ java/src/BROUILLON/src/App.java
@@ -0,0 +1,17 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		//Variable
+		int numberOfTrees = 0;
+		
+		while(numberOfTrees < 10) {
+			numberOfTrees += 1;
+			
+		System.out.println("I planted " + numberOfTrees + " trees");
+		}
+		System.out.println("I have a forest !");
+
+	}
+
+}
