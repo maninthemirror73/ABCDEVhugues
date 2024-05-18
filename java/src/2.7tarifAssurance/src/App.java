5d7c276c53f2e25e390572b5266c95f104e9bd66
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-18 06:52:55 +0200

    ...

---
+++ java/src/2.7tarifAssurance/src/App.java
@@ -0,0 +1,61 @@
+//package assurance;
+import java.util.Scanner;
+public class App {
+
+	public static void main(String[] args) {
+		// Variables
+		int ageDuConducteur;
+		int ancienneteDuPermis;
+		int nombreDaccidents;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Age du conducteur");
+		ageDuConducteur = sc.nextInt();
+		
+		System.out.println("Ancienneté du permis");
+		ancienneteDuPermis = sc.nextInt();
+		
+		System.out.println("Nombre d'accidents");
+		nombreDaccidents = sc.nextInt();
+		
+		if (ageDuConducteur < 25 && ancienneteDuPermis < 2) {
+			if (nombreDaccidents == 0) {
+			System.out.println("Tarif rouge");	
+			}
+			else {
+			System.out.println("Contrat refusé");	
+			}
+		}
+		if ((ageDuConducteur < 25 && ancienneteDuPermis > 2) || (ageDuConducteur > 25 && ancienneteDuPermis < 2)){
+			if (nombreDaccidents == 0) {
+				System.out.println("Tarif orange"); 	
+			}
+		    
+			else if (nombreDaccidents == 1) {
+				System.out.println("Tarif rouge"); 
+			}
+			else {
+				System.out.println("Contrat refusé") ;
+			} 							
+		}
+	
+		
+		if (ageDuConducteur > 25 && ancienneteDuPermis > 2) {
+			if (nombreDaccidents == 0) {
+				System.out.println("Tarif vert");
+			}
+			else if (nombreDaccidents == 1) {
+				System.out.println("Tarif orange");
+			}
+			else if (nombreDaccidents == 2) {
+				System.out.println("Tarif rouge");
+			}
+			else {
+				System.out.println("Contrat refusé");
+			}
+		}
+		sc.close();
+
+	}
+
+}
