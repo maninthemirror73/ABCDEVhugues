aaa0d8979a3ff59b4697cc1ee9ed2e7841f65bfa
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-18 06:26:23 +0200

    ...

---
+++ java/src/2.5reprographie/src/App.java
@@ -0,0 +1,38 @@
+//package reprographie;
+
+import java.util.Scanner;
+
+public class App {
+
+	final static double P1 = 0.10;
+	final static double P2 = 0.09;
+	final static double P3 = 0.08;
+
+	public static void main(String[] args) {
+
+		int nombreDeCopies;
+		double montantDeLaFacture;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Saisissez le nombre de photocopies");
+		nombreDeCopies = sc.nextInt();
+
+		if (nombreDeCopies <= 10) {
+			montantDeLaFacture = nombreDeCopies * App.P1;
+		}
+		else if (nombreDeCopies <= 30) {
+			montantDeLaFacture = 10 * App.P1 + (nombreDeCopies - 10) * App.P2;
+		}
+
+		else {
+			montantDeLaFacture = 10 * App.P1 + 20 * App.P2 + (nombreDeCopies - 30) * App.P3;
+		}
+
+		System.out.println("A payer : " + montantDeLaFacture + "€");
+
+		sc.close();
+
+	}
+}
+
