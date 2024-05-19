5453daa01e2b9222bdcff8029f5d8b591f35c035
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-19 19:53:54 +0200

    ...

--- java/src/2a.5bicyclette/src/App.java
+++ java/src/2a.5bicyclette/src/App.java
@@ -6,19 +6,45 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		
 		
 		// VARIABLES
-		
-		
-		
+		boolean beauTemps = true;
+		boolean veloOk = false;
+		boolean reparationsImmediates = false;
+		boolean livrePossede;
+		boolean livreDispoBibliotheque;
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Ma Bicyclette");
 
 		// TRAITEMENT
+if (beauTemps) {
+	
+	System.out.println("Je vais me ballader");
+	
+	if (veloOk) {
+		System.out.println("j'irai me ballader à bicyclette");
+	}
+	else {
+		System.out.println("Je vais chez le garagiste");
+		
+		if (reparationsImmediates) {
+			System.out.println("j'irai me ballader à bicyclette");	
+		}
+		else {
+			System.out.println("J'irai cueillir des joncs");
+		}
+	}
+	
+}
+else {
+	System.out.println("je vais lire un livre");
+}
 		
 		
 		
-		// AFFICHAGE RESULTAT
+		
 
 		sc.close();
 	}
