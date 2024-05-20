69fb95dc85b96c80a0213675128eb237d6139cf3
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-20 13:49:00 +0200

    Update App.java

--- java/src/BROUILLON/src/App.java
+++ java/src/BROUILLON/src/App.java
@@ -3,12 +3,16 @@ public class App {
 
 	public static void main(String[] args) {
 		//Variable
-		int i = 0;
-		while(i <= 678) {
-			System.out.println(i);
-			i++;
+		int pushUpGoal = 0;
+		while (pushUpGoal > 0) {
+			System.out.println("Push Up !");
+			pushUpGoal-= 1;
 		}
-
+		
+		do {
+			System.out.println("Push Up !");
+			pushUpGoal-= 1;
+		}while(pushUpGoal > 0);
 	}
 
 }
