d84eeed3ea2e992442dd26de6d84f36616c5159c
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-21 04:15:54 +0200

    Update App.java

--- java/src/BROUILLON/src/App.java
+++ java/src/BROUILLON/src/App.java
@@ -3,10 +3,12 @@ public class App {
 
 	public static void main(String[] args) {
 		//Variable
-		int i;
-		for(i = 56; i >= 0; i--) {
+		int i = 0;
+		while(i <= 123) {
 			System.out.println(i);
+			i++;
 		}
+
 	}
 
 }
