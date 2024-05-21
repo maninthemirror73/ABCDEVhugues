b61999cdbf5128163714b747cbc74459b7353164
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-22 01:43:10 +0200

    Update App.java

--- java/src/3.4laSomme/src/App.java
+++ java/src/3.4laSomme/src/App.java
@@ -5,30 +5,31 @@ public class App {
 		//VARIABLES
 		
 		int nombre;
-		int i;
+		int i = 1;
 		int additionDesNombres;
 		Scanner sc = new Scanner (System.in);
 		
 		//TRAITEMENT 
 		System.out.println("Entrer un nombre :");
 		nombre = sc.nextInt();
-		System.out.println("Le nombre est : " + nombre);
 		
-		for(i = 1; i <= nombre; i++) {
-			System.out.println(i);
-			additionDesNombres = i + i;
-			System.out.println("Addition des nombres = " + additionDesNombres);
+		
+		/*
+		 * for(i = 1; i <= nombre; i++) { System.out.println(i); additionDesNombres = i
+		 * + i; System.out.println("Addition des nombres = " + additionDesNombres);
+		 * 
+		 * }
+		 */
+		do {
+		i++;
+		additionDesNombres = i + i;
 			
-		}
+		} while (i<= nombre);
+		System.out.println("Addition des nombres = " + additionDesNombres);
 		
 		
 		
-		
-		
-		
-		
-		
-		
+
 		sc.close();
 	}
 
