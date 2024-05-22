79f52f4689cdfe6976d5b58c9bebb6f00f64602b
Hugues Heissler <hugues.heissler.pro@gmail.com>
2024-05-22 15:49:00 +0200

    ...

---
+++ java/src/3.5laFactorielle/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class App {
+
+	public static void main(String[] args) {
+		// VARIABLES
+		
+		int i;
+		int nombreDeDepart ;
+		int factorielle ;
+		Scanner sc = new Scanner(System.in);
+		
+	
+		
+		sc.close();
+	}
+
+}
