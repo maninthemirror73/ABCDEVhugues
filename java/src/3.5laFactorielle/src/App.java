f35744cc630fd9bbb96bf40a4f0a47bf7d0825be
Hugues Heissler <hugues.heissler.pro@gmail.com>
2024-05-22 16:38:20 +0200

    Update App.java

--- java/src/3.5laFactorielle/src/App.java
+++ java/src/3.5laFactorielle/src/App.java
@@ -6,11 +6,19 @@ public class App {
 		
 		int i;
 		int nombreDeDepart ;
-		int factorielle ;
+		int factorielle = 1 ;
 		Scanner sc = new Scanner(System.in);
 		
-	
+		//TRAITEMENT
 		
+		System.out.println("Entrer un nombre entier :");
+		nombreDeDepart = sc.nextInt();
+		
+		for (i = 1; i <= nombreDeDepart; i++) {
+			factorielle =  factorielle * i;
+		}
+		System.out.println(factorielle);
+
 		sc.close();
 	}
 
