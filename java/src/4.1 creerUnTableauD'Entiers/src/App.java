1025bee12d76ab8c248ef8716407d0089805880b
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-24 19:07:29 +0200

    ...

---
+++ java/src/4.1 creerUnTableauD'Entiers/src/App.java
@@ -0,0 +1,13 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] tableau = new int []{0,0,9,0,0,0,0};
+
+		/*
+		 * for (int i = 0; i < tableau.length; i++){ System.out.println(tableau [i]); }
+		 */
+	}
+
+}
