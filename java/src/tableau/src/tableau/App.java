0d395b0f37fd77cb481c0a95777fbd11b33c9a17
Hugues Heissler <hugues.heissler.pro@gmail.com>
2024-05-24 08:57:46 +0200

    ...

---
+++ java/src/tableau/src/tableau/App.java
@@ -0,0 +1,41 @@
+package tableau;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * String[] tableau = new String [3];
+		 * 
+		 * tableau [0] = "Bleu"; tableau [1] = "Vert"; tableau [2] = "Rouge";
+		 * System.out.println(tableau [0])
+		 */;
+		
+		//Un tableau est une variable
+		//indice
+		
+		/*
+		 * int [] tableau; tableau = new int[5];
+		 */
+		
+		int[] tableau2;
+		tableau2 = new int[] {1,3,5,7,9};
+		
+		System.out.println("Longueur de tableau2 :" + tableau2.length);
+		
+		for( int i = 0; i < tableau2.length; i++) {
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
