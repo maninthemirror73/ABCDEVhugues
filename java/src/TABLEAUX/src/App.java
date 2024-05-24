38fc32d972ee777f7019877bddb2c2790237ac39
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-24 14:54:13 +0200

    Update App.java

--- java/src/TABLEAUX/src/App.java
+++ java/src/TABLEAUX/src/App.java
@@ -3,7 +3,11 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String [] tableau = new String [10];
+		
+		for (int i = 0;i < tableau.length; i++) {
+			System.out.println(tableau[i]);
+		}
 	}
 
 }
