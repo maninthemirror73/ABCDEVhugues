5f7ece2ba205eab1227e9c2cee801adafc45fb1d
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-26 05:12:52 +0200

    ...

---
+++ java/src/3.3lesNombresSuivantsBis/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class App {
+
+	public static void main(String[] args) {
+		// VARIABLES
+		int nombreDeDepart;
+		int nombreSuivant;
+		Scanner sc = new Scanner (System.in);
+		
+		//TRAITEMENT
+		
+		System.out.println("Entrer le nombre de départ");
+		nombreDeDepart = sc.nextInt();
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
--- java/src/4.1 creerUnTableauD'Entiers/src/App.java
+++ java/src/4.1 creerUnTableauD'Entiers/src/App.java
@@ -5,9 +5,9 @@ public class App {
 		// TODO Auto-generated method stub
 		int [] tableau = new int []{0,0,9,0,0,0,0};
 
-		/*
-		 * for (int i = 0; i < tableau.length; i++){ System.out.println(tableau [i]); }
-		 */
+		
+		  for (int i = 0; i < tableau.length; i++){ System.out.println(tableau [i]); }
+		 
 	}
 
 }
