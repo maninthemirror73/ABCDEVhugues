ee4b460a38535a1364f8179a6a35109f384a3e5e
maninthemirror73 <166372191+maninthemirror73@users.noreply.github.com>
2024-05-26 05:39:46 +0200

    ...

---
+++ java/src/3.4laSommeBis/src/App.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class App {
+
+	public static void main(String[] args) {
+		//VARIABLES
+		
+		int i;
+		int nombreDeDepart;
+		int sommeDesEntiers = 1;
+		Scanner sc = new Scanner (System.in);
+		
+		//TRAITEMENT
+		
+		System.out.println("Entrer un nombre entier :");
+		nombreDeDepart = sc.nextInt();
+		
+		for(i = 0; i <= nombreDeDepart; i++) {
+			sommeDesEntiers = sommeDesEntiers + i;
+		}
+		System.out.println(sommeDesEntiers);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
