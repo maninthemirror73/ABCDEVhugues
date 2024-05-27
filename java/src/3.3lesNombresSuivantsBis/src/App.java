09f1e0dd493a23d746633ddadb79903fed1b41c6
Hugues Heissler <hugues.heissler.pro@gmail.com>
2024-05-27 08:35:00 +0200

    ...

---
+++ java/src/3.3lesNombresSuivantsBis/src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class App {
+
+	public static void main(String[] args) {
+		//VARIABLES
+		
+		int nombreDeDepart;
+		int i = 1;
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Entrer un nombre entier");
+		nombreDeDepart = sc.nextInt();
+		
+		/*
+		 * while (i <= 10) { System.out.println(nombreDeDepart + i); i++; }
+		 */
+		
+		for (i  =1; i <= 10; i++) {
+			System.out.println(nombreDeDepart + i);
+		}
+
+		
+		sc.close();
+
+	}
+
+}
--- java/src/tableau/src/tableau/App.java
+++ java/src/tableau/src/tableau/App.java
@@ -19,12 +19,16 @@ public class App {
 		 */
 		
 		int[] tableau2;
-		tableau2 = new int[] {1,3,5,7,9};
+		tableau2 = new int[] {1,3,5};
 		
 		System.out.println("Longueur de tableau2 :" + tableau2.length);
 		
+		tableau2[0] = 3;
+		tableau2[1] = 5;
+		tableau2[2] = 22;
+		
 		for( int i = 0; i < tableau2.length; i++) {
-			System.out.println(i);
+			
 		}
 		
 		
