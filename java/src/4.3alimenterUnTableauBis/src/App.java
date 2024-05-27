2bac1a5c3bf348340f01195989c762171f5201fa
Hugues Heissler <hugues.heissler.pro@gmail.com>
2024-05-27 15:57:25 +0200

    ...

--- java/src/4.3alimenterUnTableauBis/src/App.java
+++ java/src/4.3alimenterUnTableauBis/src/App.java
@@ -1,8 +1,36 @@
-
+import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//VARIABLES
+		int i ;
+		int tailleDuTableau;
+		int positif = 0;
+		int negatif = 0;
+		int []tableau;
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Entrez le nombre de valeurs que vous souhaitez saisir :");
+		tailleDuTableau = sc.nextInt();
+		
+		tableau = new int [tailleDuTableau];
+		
+		for (i = 0; i < tailleDuTableau; i++) {
+			System.out.println("Saisissez la valeur n°" + (i+1));
+			tableau [i] = sc.nextInt();
+			
+			if (tableau [i] >= 0){
+				positif++;
+			}
+			else {
+				negatif++;
+			}
+		}//FIN FOR
+		
+		System.out.println("Vous avez  saisi" + positif + "nombres positifs");
+		System.out.println("Vous avez  saisi" + negatif + "nombres negatifs");
+
+		sc.close();
 
 	}
 
