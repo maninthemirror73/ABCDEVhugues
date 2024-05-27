779709706044f4f5fbb52f33fea6e3a8daec089b
Hugues Heissler <hugues.heissler.pro@gmail.com>
2024-05-27 14:41:59 +0200

    ...

--- java/src/4.3alimenterUnTableau/src/App.java
+++ java/src/4.3alimenterUnTableau/src/App.java
@@ -1,26 +1,38 @@
 import java.util.Scanner;
-
 public class App {
 
 	public static void main(String[] args) {
-		//VARIABLES
-		
-		int nombreDeValeursASaisir;
-		int i ;
+		// VARIABLES
 		Scanner sc = new Scanner (System.in);
-		
-		System.out.println("Entrer un entier :");
-		nombreDeValeursASaisir = sc.nextInt();
-		
+		int tailleDuTableau;
+		int i;
+		int positif = 0;
+		int negatif = 0;
 		int [] tableau;
-		tableau = new int [] {};
 		
+		System.out.println("Entre le nombre de valeurs que vous souhaitez saisir :");
+		tailleDuTableau = sc.nextInt();
 		
+		tableau = new int [tailleDuTableau];
 		
+		for(i = 0; i < tailleDuTableau; i++) {
+			System.out.println("Saisissez la valeur N°" + (i+1));
+			tableau [i] = sc.nextInt();
+			
+			if(tableau [i] >= 0) {
+				positif++;
+			}
+			else {
+				negatif++;
+			}
+		} //FIN FOR
 		
-		
-		
+		System.out.println("Vous avez saisi " + positif + "nombres positifs");
+		System.out.println("Vous avez saisi " + negatif + "nombresnegatifs");
 		sc.close();
+
 	}
 
 }
+
+
