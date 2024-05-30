e3314271dbc0effa362e83de556698e29ba21e28
Hugues Heissler <hugues.heissler.pro@gmail.com>
2024-05-30 15:58:50 +0200

    ...

---
+++ java/src/insertionDansUnTableauOrdonne/src/insertionDansUnTableauOrdonne/App.java
@@ -0,0 +1,34 @@
+package insertionDansUnTableauOrdonne;
+import java.util.Scanner;
+import java.util.Arrays;
+public class App {
+
+	public static void main(String[] args) {
+		//VARIABLES
+		/*
+		 * int tailleDuTableau; int i; int nombreAInserer; int [] tableauInitial;
+		 * Scanner sc = new Scanner (System.in);
+		 */
+		
+		int[] numbers = new int [] {8, 2, 7, 3, 1, 5};
+		
+		Arrays.sort(numbers);
+		
+        System.out.println("Sorted Numbers:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+		
+		
+		
+
+		
+
+	       
+		
+
+		
+	}
+
+}
