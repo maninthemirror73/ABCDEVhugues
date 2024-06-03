8cc4de5a1b075f369e402fcb8842bbf79d85466e
Hugues Heissler <hugues.heissler.pro@gmail.com>
2024-06-03 14:42:27 +0200

    ...

--- java/src/evaluation3/src/evaluation3/App1.java
+++ java/src/evaluation3/src/evaluation3/App1.java
@@ -1,34 +1,40 @@
 package evaluation3;
-
 import java.util.Scanner;
-public class App {
+public class App1 {
 
 	public static void main(String[] args) {
-		// VARIABLES
+		//VARIABLES
+		
 		Scanner sc = new Scanner (System.in);
 		int []tableau;
-		double moyenne;
-		int i;
 		tableau = new int [] {2,4,1,8,6,14,23,25,7,42};
 		int somme = 0;
+		double moyenne;
 		int plusGrand = 0;
+		int i;
 		
 		for (i = 0; i < tableau.length; i++) {
-			
-			somme += tableau[i];
+			somme = somme + tableau[i];
 			
 			if (plusGrand < tableau[i]) {
-				
 				plusGrand = tableau[i];
-			}	
-			
+			}
 		}
-		
 		moyenne = somme/tableau.length;
-		int carreDePlusGrand = plusGrand * plusGrand;
+		int carrePlusGrand =  plusGrand * plusGrand;
 		
 		System.out.println("La moyenne est : " + moyenne);
-		System.out.println("Le carre de plusGrand est :" + carreDePlusGrand);
+		System.out.println("Le carre de plusGrand est :" + carrePlusGrand);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 		sc.close();
 	}
